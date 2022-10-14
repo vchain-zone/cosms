4df128cfd481d81a27aeb64164a338be779dd6ad
Phạm Nam <nam.pv1805@gmail.com>
2022-10-14 17:48:19 +0700

    feat: add function uploadWasm

--- src/lib/wallet/index.ts
+++ src/lib/wallet/index.ts
@@ -1,6 +1,7 @@
 import {
   SigningCosmWasmClient,
   SigningCosmWasmClientOptions,
+  UploadResult,
 } from '@cosmjs/cosmwasm-stargate';
 import {
   DirectSecp256k1HdWallet,
@@ -9,14 +10,20 @@ import {
 } from '@cosmjs/proto-signing';
 import { AccountData } from '@cosmjs/proto-signing/build/signer';
 import {
+  calculateFee,
+  GasPrice,
   SigningStargateClient,
   SigningStargateClientOptions,
+  StdFee,
 } from '@cosmjs/stargate';
-import { Tendermint34Client } from '@cosmjs/tendermint-rpc';
-import { HttpEndpoint } from '@cosmjs/tendermint-rpc/build/rpcclients';
+
+import Cosm from '../cosm';
 import { provider } from '../providers';
 
-import { TendermintBatchClient } from '../tendermint-rpc/tendermintbatchclient';
+const defaultUploadGas = 2500000;
+const defaultInitGas = 1000000;
+const defaultExecGas = 500000;
+const defaultGasPrice = 0.25;
 
 export default OfflineSigner;
 
@@ -30,6 +37,7 @@ export class Wallet {
   private _cosmWasmSigner: SigningCosmWasmClient;
   private _stargateSigner: SigningStargateClient;
   private _account: AccountData;
+  private _denom: string;
 
   public static async getWalletFromMnemonic(
     provider: provider,
@@ -52,7 +60,13 @@ export class Wallet {
       options.stargateOptions
     );
     const [account] = await wallet.getAccounts();
-    return new Wallet(wallet, account, cosmWasmClient, stargateClient);
+    return new Wallet(
+      wallet,
+      account,
+      cosmWasmClient,
+      stargateClient,
+      await getDenom(provider)
+    );
   }
 
   public static async getWalletsFromMnemonic(
@@ -87,7 +101,13 @@ export class Wallet {
         options.stargateOptions
       );
       results.push(
-        new Wallet(wallets, accounts[i], cosmWasmClient, stargateClient)
+        new Wallet(
+          wallets,
+          accounts[i],
+          cosmWasmClient,
+          stargateClient,
+          await getDenom(provider)
+        )
       );
     }
     return results;
@@ -113,7 +133,13 @@ export class Wallet {
         options.stargateOptions
       );
       results.push(
-        new Wallet(signer, accounts[i], cosmWasmClient, stargateClient)
+        new Wallet(
+          signer,
+          accounts[i],
+          cosmWasmClient,
+          stargateClient,
+          await getDenom(provider)
+        )
       );
     }
     return results;
@@ -123,15 +149,43 @@ export class Wallet {
     signer: OfflineSigner,
     account: AccountData,
     cosmWasmSigner: SigningCosmWasmClient,
-    stargateSigner: SigningStargateClient
+    stargateSigner: SigningStargateClient,
+    denom: string
   ) {
     this._signer = signer;
     this._account = account;
     this._cosmWasmSigner = cosmWasmSigner;
     this._stargateSigner = stargateSigner;
+    this._denom = denom;
+  }
+
+  public async uploadWasm(wasmCode: Uint8Array, fee?: StdFee, memo?: string): Promise<UploadResult> {
+    fee = fee == null ? this.getFee(defaultUploadGas, defaultGasPrice) : fee;
+    return await this._cosmWasmSigner.upload(this.address, wasmCode, fee, memo);
+  }
+
+  public async deloyContractFromCodeId(codeId: number){
+
+  }
+
+  public getFee(gas: number, gasPrice: number): StdFee {
+    return calculateFee(
+      gas,
+      GasPrice.fromString(gasPrice.toString() + this._denom)
+    );
   }
 
   get address(): string {
     return this._account.address;
   }
+
+  get denom(): string {
+    return this._denom;
+  }
+}
+
+async function getDenom(provider: provider): Promise<string> {
+  const cosm = new Cosm(provider);
+  const queryRs = await cosm.cosmos.mint.query.Params({});
+  return queryRs.params.mintDenom;
 }
